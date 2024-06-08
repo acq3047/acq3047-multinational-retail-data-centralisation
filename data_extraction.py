ea5c0717398d449e1139cba6c5ce77c50c2a6124
acq3047 <acq3047@gmail.com>
2024-06-08 20:24:41 +0100

    Create data_extraction.py file

---
+++ data_extraction.py
@@ -0,0 +1,14 @@
+import boto3
+import pandas
+import numpy
+import csv
+
+class DataExtractor:
+    """
+    This class will work as a utility class, in it you will be creating methods
+    that help extract data from different data sources.
+    The methods contained will be fit to extract data from a particular data source,
+    these sources will include CSV files, an API and an S3 bucket.
+    """
+    def __init__(self):
+        pass
