b24d80d49e2968514db475a07b0525c9988ee211
acq3047 <acq3047@gmail.com>
2024-06-08 20:39:57 +0100

    Create data_cleaning.py file

---
+++ data_cleaning.py
@@ -0,0 +1,11 @@
+import boto3
+import pandas
+import numpy
+import csv
+
+class DataCleaning:
+    """
+    This class will contain methods to clean data from each of the data sources.
+    """
+    def __init__(self):
+        pass
