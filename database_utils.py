b2475db246adb79f3e5764402cbaf676e2b80ae9
acq3047 <acq3047@gmail.com>
2024-06-08 20:38:34 +0100

    Create database_utils.py file

---
+++ database_utils.py
@@ -0,0 +1,11 @@
+import boto3
+import pandas
+import numpy
+import csv
+
+class DatabaseConnector:
+    """
+    This class will be use to connect with and upload data to the database.
+    """
+    def __init__(self):
+        pass
