b559a185278edbf498c27cedb80def39825bf468
acq3047 <acq3047@gmail.com>
2024-06-11 10:51:11 +0100

    Include API testing

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from database_utils import DatabaseConnector
 from data_extraction import DataExtractor
 from data_cleaning import DataCleaning
+import requests
 
 if __name__ == "__main__":
     connector = DatabaseConnector()
@@ -37,6 +38,35 @@ if __name__ == "__main__":
     # Upload cleaned data to the dim_card_details table
     print('Upload the dim_card_details table')
     connector.upload_to_db(cleaned_card_df, 'dim_card_details')
+
+
+     # API details
+    api_key = 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'
+    headers = {
+        'x-api-key': api_key
+    }
+    number_stores_endpoint = 'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores'
+
+    # Get number of stores
+    try:
+        number_of_stores = extractor.list_number_of_stores(number_stores_endpoint, headers)
+        print(f"Number of stores: {number_of_stores}")
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error occurred: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    store_number_endpoint ='https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
+      # Get the store number
+    try:
+        store_number_df = extractor.retrieve_stores_data(store_number_endpoint, headers, number_of_stores)
+        print("Extracted Store Data:")
+        print(store_number_df)
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error occurred: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 
 
 
