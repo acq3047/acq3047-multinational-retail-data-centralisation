67148c1eb5b5c8535820c5609590127c692f88a3
acq3047 <acq3047@gmail.com>
2024-06-23 20:06:14 +0200

    Add more querying tasks

--- querying_data_sql.sql
+++ querying_data_sql.sql
@@ -29,9 +29,52 @@ ORDER BY price DESC;
 SELECT *
 FROM dim_store_details
 SELECT *
-FROM dim_products
-SELECT *
 FROM orders_table
-SELECT *
-FROM dim_card_details --SELECT count(o.product_quantity) AS product_quantity_count,
-    --    s.store_type as location,
+SELECT count(o.product_quantity) AS product_quantity_count,
+    CASE
+        WHEN s.store_type = 'Website' THEN 'Web'
+        ELSE 'Offline'
+    END AS location,
+    --COUNT(s.store_type) as location,
+    SUM(p.product_price * o.product_quantity) as price
+FROM orders_table o
+    JOIN dim_products p ON o.product_code = p.product_code
+    JOIN dim_store_details s ON o.store_code = s.store_code
+GROUP BY location
+ORDER BY location;
+-- Task 5: What percentage of sales comes from each store type?
+WITH total_sales_by_store_type AS (
+    SELECT s.store_type,
+        SUM(p.product_price * o.product_quantity) AS total_sales
+    FROM orders_table o
+        JOIN dim_products p ON o.product_code = p.product_code
+        JOIN dim_store_details s ON o.store_code = s.store_code
+    GROUP BY s.store_type
+),
+total_sales_all AS (
+    SELECT SUM(total_sales) AS grand_total_sales
+    FROM total_sales_by_store_type
+)
+SELECT ts.store_type,
+    ts.total_sales,
+    (ts.total_sales / ta.grand_total_sales * 100) AS percentage_total
+FROM total_sales_by_store_type ts,
+    total_sales_all ta
+ORDER BY ts.total_sales DESC;
+-- Task 6: Which month in each year produced the highest cost sales
+SELECT SUM(p.product_price * o.product_quantity) AS total_sales,
+    d.year as year,
+    d.month as month
+FROM orders_table o
+    JOIN dim_date_times d ON o.date_uuid = d.date_uuid
+    JOIN dim_products p ON o.product_code = p.product_code
+GROUP BY d.year,
+    d.month
+ORDER BY total_sales DESC
+Limit 10;
+-- Task 7: What is your staff headcount?
+SELECT SUM(s.staff_numbers) AS total_staff_numbers,
+    s.country_code
+FROM dim_store_details s
+GROUP BY s.country_code
+ORDER BY total_staff_numbers DESC;
